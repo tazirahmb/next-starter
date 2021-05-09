5eb74c50046dbcb6531a9b6d496d40f6621e97bb
Tazirahmb <tazirahmb@outlook.com>
2021-05-09 11:53:59 +0700

    add storybook

--- babel.config.js
+++ babel.config.js
@@ -1,47 +1,38 @@
 module.exports = {
   env: {
     development: {
-      presets: ['@babel/preset-env', '@babel/preset-react'
-      ],
+      presets: ['@babel/preset-env', '@babel/preset-react'],
       plugins: [
-        ['styled-components',
-          { ssr: true, displayName: true
-          }
-        ],
-        ['@babel/plugin-proposal-class-properties'
-        ]
-      ]
+        // ['styled-components',
+        //   { ssr: true, displayName: true
+        //   }
+        // ],
+        ['@babel/plugin-proposal-class-properties'],
+      ],
     },
     production: {
       presets: [
-        ['@babel/preset-env',
-          { targets: { ie: 9, uglify: true
-            }
-          }
-        ],
-        '@babel/preset-react'
+        ['@babel/preset-env', { targets: { ie: 9, uglify: true } }],
+        '@babel/preset-react',
       ],
       plugins: [
-        ['styled-components',
-          { ssr: true, displayName: true
-          }
-        ],
-        ['@babel/plugin-proposal-class-properties'
-        ]
-      ]
+        // ['styled-components',
+        //   { ssr: true, displayName: true
+        //   }
+        // ],
+        ['@babel/plugin-proposal-class-properties'],
+      ],
     },
     test: {
-      presets: ['@babel/preset-env', '@babel/preset-react'
-      ],
+      presets: ['@babel/preset-env', '@babel/preset-react'],
       plugins: [
-        ['styled-components',
-          { ssr: true, displayName: true
-          }
-        ],
-        ['@babel/plugin-proposal-class-properties'
-        ],
-        '@babel/plugin-transform-runtime'
-      ]
-    }
-  }
+        // ['styled-components',
+        //   { ssr: true, displayName: true
+        //   }
+        // ],
+        ['@babel/plugin-proposal-class-properties'],
+        '@babel/plugin-transform-runtime',
+      ],
+    },
+  },
 };
